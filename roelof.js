32a9ab1a099deea6da7d0f8df4138f7e2fab6294
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2022-10-17 22:05:55 +0200

    animation seems to work

--- roelof.js
+++ roelof.js
@@ -5,22 +5,17 @@ buttons = document.querySelectorAll('.faq-btn');
 buttons.forEach(button => {
     button.addEventListener('click', (e) =>{
         let target = e.currentTarget;
-        console.log(e.currentTarget)
-
-        let icon = target.querySelector(img); 
         let collapsed = (e.currentTarget).getAttribute('aria-expanded'); 
         if (collapsed === "true"){
             target.setAttribute('aria-expanded', 'false');
             
+            
         }
         else {
          
-            target.setAttribute('aria-expanded', 'true'); 
+            target.setAttribute('aria-expanded', 'true');
         }
-
-         
-        console.log(icon);
-    
+   
     })
 });  
 
