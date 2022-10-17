85a1f9fe2ecf21f6c47e6c2f002c36c89f6387a6
RoelofWobben <r.wobben@home.nl>
2022-10-17 11:42:50 +0200

    open status working

--- roelof.js
+++ roelof.js
@@ -2,4 +2,24 @@ buttons = document.querySelectorAll('.faq-btn');
 
 // make the eventListeners 
 
+buttons.forEach(button => {
+    button.addEventListener('click', () =>{
+    
+        let x = button.getAttribute("aria-expanded");
+        var tekst_id = button.getAttribute("aria-controls");
+        var tekst_element = document.getElementById(tekst_id);
+
+        if (x === "true"){
+            x = "false";
+            tekst_element.setAttribute("hidden","true");
+        } else {
+            x = "true";
+            tekst_element.removeAttribute("hidden");
+        }
+        button.setAttribute("aria-expanded", x);  
+    })
+});  
+
+
+        
 
